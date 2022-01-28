8c0420a2cca7d410f1ce423ac63bd392722fb8f1
Shareef-deen <49625061+Shareef-deen@users.noreply.github.com>
2022-01-28 11:03:28 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+var multer = require('multer');
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse',multer().single('upfile'), (req, res)=>{
+  console.log(req.file)
+  console.log(req.file.originalname)
+  let fname = req.file.originalname;
+  let ftype = req.file.mimetype;
+  let fsize = req.file.size;
+  res.json({
+    name: fname,
+    type: ftype,
+    fsize: fsize
+    });
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
